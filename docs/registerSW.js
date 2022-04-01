272753e1f5a3265068fda102815bb73dd861d8cd
Erik Hellman <erik.hellman@iteam.se>
2022-04-01 08:48:41 +0200

    app manifest

---
+++ docs/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/paginated-markdown-demo/sw.js', { scope: '/paginated-markdown-demo/' })})}
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: 'paginated-markdown-demo',
+  base: '/paginated-markdown-demo/',
   build: {
     outDir: 'docs',
     sourcemap: true,
   },
+  plugins: [
+    VitePWA({
+        manifest: {
+            display: 'fullscreen'
+        }
+    })
+  ],
 });
