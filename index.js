6b3d799e7c0d3fe20e1754ec43fc0e36d8a03a32
Erik Hellman <erik.hellman@iteam.se>
2022-06-02 15:22:34 +0200

    chapter break before each h1

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ import './index.css';
 import { marked } from 'marked';
 import content from './content.md?raw';
 
+const renderer = {
+  heading(text, level) {
+    return `<hr class="hard-break" /><p class="heading${level}">${text}</p>`;
+  },
+};
+marked.use({ renderer });
+
 const htmlContent = marked.parse(content);
 const contentWrapper = document.querySelector('#content');
 contentWrapper.addEventListener('scroll', (e) => {
   console.log(`Scroll!`, e);
 });
-contentWrapper.innerHTML = `
-  ${htmlContent}
-  <hr class="hard-break" />
-  ${htmlContent}
-  <hr class="hard-break" />
-  ${htmlContent}
-`;
+contentWrapper.innerHTML = `${htmlContent}`;
